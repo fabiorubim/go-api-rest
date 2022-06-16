f8a82f9ae587501d8111dcbdea89ab8a2ef2b71e
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-06-15 21:17:37 -0300

    Criado, deletando e editando com Gorm, ORM da linguagem Go.

--- routes/routes.go
+++ routes/routes.go
@@ -13,5 +13,8 @@ func HandleRequest() {
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("GET")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("POST")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("DELETE")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
